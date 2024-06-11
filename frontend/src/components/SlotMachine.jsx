4c4df47ee2d99ddd7af078840c48f0f9af90dccb
phxdablio <pedro.11gustavo.7@gmail.com>
2024-06-11 00:28:12 -0300

    slot machine feito

--- frontend/src/components/SlotMachine.jsx
+++ frontend/src/components/SlotMachine.jsx
@@ -1,38 +1,44 @@
-import style from './slotMachine.module.css'
+import style from './slotMachine.module.css';
 
 export function SlotMachine() {
   return (
-    <>
-      <div className={style.jogoBackground}>
-        <div className={style.jogoContainer}>
-          <div className={style.jogoSlotMachine}>
-            <div className={style.jogoSlot + ' ' + style.jogoHighlighted}>
-              <img src="chip.png" alt="Chip" />
-            </div>
-            <div className={style.jogoSlot}>
-              <img src="chip.png" alt="Chip" />
-            </div>
-            <div className={style.jogoSlot}>
-              <img src="chip.png" alt="Chip" />
-            </div>
+    <div className={style.jogoBackground}>
+      <div className={style.jogoContainer}>
+        <div className={style.jogoSlotMachine}>
+          <div className={style.jogoSlot}>
+            <img src="chip.png" alt="Chip" />
           </div>
-          <div className={style.jogoControls}>
-            <div className={style.jogoBalance}>
-              <span>Saldo</span>
-              <input type="text" readOnly />
-            </div>
-            <div className={style.jogoBet}>
-              <button>-</button>
-              <span>Valor por aposta</span>
-              <button>+</button>
-            </div>
+          <div className={style.jogoSlot}>
+            <img src="chip.png" alt="Chip" />
           </div>
-          <div className={style.jogoPlayButton}>
-            <button>Play</button>
+          <div className={style.jogoSlot}>
+            <img src="chip.png" alt="Chip" />
           </div>
-          <button className={style.jogoExitButton}>Sair</button>
+        </div>
+        <div className={style.jogoControls}>
+          <div className={style.jogoBalance}>
+            <span>Saldo: R$</span>
+            <input type="text" readOnly value="100" />
+          </div>
+          <div className={style.jogoBet}>
+            <button>-</button>
+            <input 
+              type="text" 
+              placeholder="Digite o valor da aposta" 
+              inputMode="numeric"
+              onInput={(e) => e.target.value = e.target.value.replace(/[^0-9]/g, '')}
+            />
+            <button>+</button>
+          </div>
+        </div>
+        <div className={style.jogoButtons}>
+          <div className={style.jogoButtonGroup}>
+            <button className={style.jogoPlayButton}>Play</button>
+            <button className={style.jogoExitButton}>Sair</button>
+          </div>
+          <button className={style.jogoDeleteButton}>Deletar Saldo</button>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
