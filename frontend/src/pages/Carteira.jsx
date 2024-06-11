720b6d7309702d4a2e81917bea398bd51040ef52
phxdablio <pedro.11gustavo.7@gmail.com>
2024-06-11 01:54:42 -0300

    Informações de usuarios

--- frontend/src/pages/Carteira.jsx
+++ frontend/src/pages/Carteira.jsx
@@ -1,40 +1,24 @@
+import React from 'react';
+import styles from './carteira.module.css';
+
 export function Carteira() {
     return (
-        <>
-            <main>
-                <div className="text-center"> {/* Adicionando a classe text-center para centralizar as imagens */}
-                    <div>
-                        <div>
-                            <div className="row g-2">
-                                <div className="col-6">
-                                    <div className="p-6">
-                                        <div className="card" alt="Depositar">
-                                            <img src="src/img/carteira.webp" alt="imagemSapoNaCarteira"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Depositar</h5>
-                                                <p className="card-text">Deposite seu dinheiro atraves de um cartão para começar a jogar.</p>
-                                                <a href="/Deposito" className="btn btn-primary">Ir para depósito</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-6">
-                                    <div className="p-6">
-                                        <div className="card" alt="Informações bancárias">
-                                            <img src="src/img/002.webp" alt="imagemSaposNaMaquinaDeAposta"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Informações bancárias</h5>
-                                                <p className="card-text">Edite seus dados bancários.</p>
-                                                <a href="#" className="btn btn-primary">Ir para edição</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </>
+        <div className={styles.container}>
+            <div className={styles.header}>Gerenciamento de Conta</div>
+            <div className={styles.form}>
+                <label className={styles.label}>
+                    Nome Exibido
+                    <input type="text" className={styles.input} placeholder="Seu nome aqui" disabled />
+                </label>
+                <label className={styles.label}>
+                    Data Nascimento
+                    <input type="text" className={styles.input} placeholder="00/00/0000" disabled />
+                </label>
+            </div>
+            <div className={styles.buttons}>
+                <button className={styles.editButton}>Editar Informações</button>
+                <button className={styles.deleteButton}>Excluir Conta</button>
+            </div>
+        </div>
     );
 }
